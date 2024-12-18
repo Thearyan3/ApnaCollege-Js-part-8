ededf6fecd8fb83e6a934bf6c3f904576dbfdb54
Aryan <chaudharyaryan3456@gmail.com>
2024-12-18 22:15:16 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 // 1. Array methods :- 
 // (i). forEach --> 
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((el) => {
+//     console.log(el);
+// })
+// let print = function(el) {
+//     console.log(el);
+// }
+// arr.forEach(print);
+
+// let arr1 = [{
+//     name: "Aryan", 
+//     marks: 95
+// },
+//  {
+//     name: "Tarun", 
+//     marks: 59
+// },
+//  {
+//     name: "Arun", 
+//     marks: 81
+// }];
+// arr1.forEach((student) => {
+//     console.log(student);
+// });
+// arr1.forEach((student) => {
+//     console.log(student.marks);
+// });
 // (ii). map --> 
 // (iii). filter --> 
 // (iv). some --> 
