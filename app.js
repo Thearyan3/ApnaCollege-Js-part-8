3b2306b83defc2476a2dc996eaef626e50e07d19
Aryan <chaudharyaryan3456@gmail.com>
2024-12-19 18:23:06 +0530

    updated

--- app.js
+++ app.js
@@ -58,3 +58,11 @@
 // });
 // console.log(finalVal);
 
+let arr = [1, 2, 3, 4, 5, 6, 9, 11, 2, 3, 1];
+// let max = -1;
+// for(let i=0; i<arr.length; i++){
+//     if(max < arr[i]){
+//          max = arr[i];
+//     }
+// }
+// console.log(max);
